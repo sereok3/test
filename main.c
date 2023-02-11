ca3e1882a02f95495b7b889396fdf1d1718663ff
sereok3 <103284422+sereok3@users.noreply.github.com>
2023-02-11 19:38:27 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,32 +6,10 @@ int main(int argc, char *argv[]){
   if (argc == 1)
     return 1;
 
-  int result = 1;
-  for (int i = 0; i < 3; i++) {
-    result *= *argv[1];
-  }
-
-  printf("\n");
-  printf(" _____          _          ____\n");
-  printf("|  ___| __ ___ (_)_ __    / ___|  ___ __ _ _ __  _ __   ___ _ __\n");
-  printf("| |_ | '__/ _ \\| | '_ \\  | |  _ / _ \\ / _` | '_ \\| '_ \\ / _ \\ '__|\n");
-  printf("|  _|| | | (_) | | | | | | |_| | (_) | (_| | | | | | | |  __/ |\n");
-  printf("|_|  |_|  \\___/|_|_| |_|  \\____|\\___/ \\__,_|_| |_|_| |_|\\___|_|\n");
-  printf("\n");
-
   char *from = malloc(strlen(argv[1]) + 1);
-  char *to = malloc(strlen(argv[1]) + 1);
   strcpy(from,argv[1]);
-  char *save = from;
-  char *save2 = to;
-  while (*from){
-    *to++ = *from++;
-  }
-  *to='\0';
-  from = save;
-  to = save2;
 
-  printf(to);
+  printf(from);
 
   return 0;
 }
